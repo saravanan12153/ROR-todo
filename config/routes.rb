04bf00e3a282f7b2470ff690f70de4be56ee5191
Sulman Baig <sulmanpersonal@gmail.com>
2015-02-14 23:59:11 +0100

    Added ABOUT page to pages controller and set path to root/about

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
 
+  get 'about' => 'pages#about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
